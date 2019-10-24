c93c33293feac51f2f6fe148b1c2131b1ad5ad0d
Lukie <lss5@cin.ufpe.br>
2019-10-24 18:40:07 -0300

    AddingTheorem at Cartesian Tree

--- src/Tree.v
+++ src/Tree.v
@@ -1,3 +1,5 @@
+Require Import List.
+
 Inductive tree : Type :=
 | leaf : tree
 | node : nat -> tree -> tree -> tree.
@@ -15,3 +17,9 @@ Definition merge : tree -> tree -> tree.
 Admitted.
 
 Definition empty : tree := leaf.
+
+Definition flatten : tree -> list nat.
+Admitted.
+
+Theorem in_order_traversal : forall (l : list nat), (l = (flatten (create_cartesian_tree l))).
+Proof. Admitted.
